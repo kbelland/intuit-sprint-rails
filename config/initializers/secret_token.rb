d55792cf7f9738cb98469aad8dc3451748fd416b
Matt Belland <kristopher.belland@students.olin.edu>
2011-02-28 14:59:34 -0500

    Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Futurepath::Application.config.secret_token = '69c78acd46b7c27ee3b4d75ac84f6db6900c8ce8dd841388a07ed50a4cb8f29ddce3a9148446ab951a8d0b07c383bab01d601695bae9e73f032fe6633c92fa2f'
